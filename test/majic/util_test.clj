99abefb5103e9436c8c7556987bc754878af5180
emiln <emil@nauerby.com>
2014-03-05 22:11:07 +0100

    Test the utility functions
    This introduces a few simple tests for the functions in the `majic.util` namespace.

---
+++ test/majic/util_test.clj
@@ -0,0 +1,26 @@
+(ns majic.util-test
+  (:require [clojure.test :refer :all]
+  	        [midje.sweet :refer :all]
+            [majic.util :refer :all]))
+
+(deftest util-test
+  (facts "About string->keyword"
+    (fact "It should work for trivial examples."
+      (string->keyword "White") => :white
+      (string->keyword "Tap") => :tap)
+    (fact "It should work for more complex examples."
+      (string->keyword "Black or Green") => :black-or-green
+      (string->keyword "A tricky ol' string") => :a-tricky-ol'-string))
+  (facts "About string->int"
+  	(fact "It should parse a bunch of numbers."
+  	  (string->int "0") => 0
+  	  (string->int "123") => 123
+  	  (string->int "9013593195") => 9013593195)
+  	(fact "It should return nil if supplied garbage."
+  	  (string->int "herpaderpa") => nil
+  	  (string->int "") => nil))
+  (facts "About n-partitions"
+  	(fact "It should partition a small range properly."
+  	  (n-partitions 5 (range 10)) => [[0 5] [1 6] [2 7] [3 8] [4 9]])
+  	(fact "It should partition unevenly if necessary."
+  	  (n-partitions 3 (range 10)) => [[0 3 6 9] [1 4 7] [2 5 8]])))
